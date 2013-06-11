72cb20dd68479d6cfe2af95fe22b1ea5cd09f9ef
Graham Hay <grahamrhay@gmail.com>
2013-06-11 19:35:52 +0100

    Add feeds to list

--- src/erl_reader.erl
+++ src/erl_reader.erl
@@ -1,9 +1,14 @@
 -module(erl_reader).
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start/0, start_link/0, add_feed/1]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {feeds=[]}).
+
+start() ->
+    application:start(?MODULE).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -11,7 +16,10 @@ start_link() ->
 init([]) ->
     process_flag(trap_exit, true),
     io:format("~p starting~n", [?MODULE]),
-    {ok, []}.
+    {ok, #state{}}.
+
+handle_call({add, Uri}, _From, #state{feeds=F}) ->
+    {reply, ok, #state{feeds=[Uri|F]}};
 
 handle_call(_, _From, State) ->
     {reply, ok, State}.
@@ -28,3 +36,6 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+add_feed(Uri) ->
+    gen_server:call(?MODULE, {add, Uri}).
