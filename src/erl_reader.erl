5f35708760afb21a315755bbb6e26df4b6ace11c
Graham Hay <grahamrhay@gmail.com>
2014-09-28 15:16:38 +0100

    Removed calls that don't make sense anymore

--- src/erl_reader.erl
+++ src/erl_reader.erl
@@ -1,7 +1,7 @@
 -module(erl_reader).
 -behaviour(gen_server).
 
--export([start/0, start_link/0, add_feed/2, import_feed_list/2, list_feeds/0, update/0, update_feed/1]).
+-export([start/0, start_link/0, add_feed/2, import_feed_list/2]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
@@ -26,7 +26,6 @@ start_link() ->
 
 init([]) ->
     io:format("~p starting~n", [?MODULE]),
-    %erlang:send_after(?INTERVAL, self(), check_for_updates),
     {ok, #state{}}.
 
 handle_call({add, User, Uri}, _From, #state{feeds=Feeds}=State) ->
@@ -42,17 +41,6 @@ handle_call({import, User, File}, _From, State) ->
         throw:Reason -> {reply, Reason, State}
     end;
 
-handle_call(list, _From, #state{feeds=Feeds}=State) ->
-    {reply, get_all_feeds(Feeds), State};
-
-handle_call(update, _From, #state{feeds=Feeds}=State) ->
-    check_for_updates(Feeds),
-    {reply, ok, State};
-
-handle_call({update_feed, Uri}, _From, #state{feeds=Feeds}=State) ->
-    update_feed(Uri, Feeds),
-    {reply, ok, State};
-
 handle_call(_, _From, State) ->
     {reply, ok, State}.
 
@@ -62,12 +50,6 @@ handle_cast({add, User, Uri}, #state{feeds=Feeds}=State) ->
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
-
-handle_info(check_for_updates, #state{feeds=Feeds}=State) ->
-    io:format("Checking for updates~n"),
-    check_for_updates(Feeds),
-    erlang:send_after(?INTERVAL, self(), check_for_updates),
-    {noreply, State};
 
 handle_info(_Msg, State) ->
     {noreply, State}.
@@ -84,18 +66,6 @@ add_feed(User, Uri) ->
 
 import_feed_list(User, File) ->
     gen_server:call(?MODULE, {import, User, File}).
-
-list_feeds() ->
-    gen_server:call(?MODULE, list).
-
-update() ->
-    gen_server:call(?MODULE, update).
-
-update_feed(Uri) ->
-    gen_server:call(?MODULE, {update_feed, Uri}).
-
-get_all_feeds(Feeds) ->
-    lists:map(fun({_,Feed}) -> Feed end, Feeds).
 
 add_user_to_feed(User, Uri) ->
     gen_server:call(er_user_feeds, {add, User, Uri}).
@@ -122,13 +92,3 @@ create_feed(Uri, Feeds) ->
                 {error, Error} -> {Error, Feeds}
             end
     end.
-
-check_for_updates(Feeds) ->
-    NeedingUpdate = lists:filter(fun(Feed) -> Feed#er_feed.nextCheck < calendar:universal_time() end, get_all_feeds(Feeds)),
-    io:format("Feeds needing update: ~p~n", [length(NeedingUpdate)]),
-    lists:foreach(fun(Feed) -> supervisor:start_child(er_crawler_sup, [Feed]) end, NeedingUpdate).
-
-update_feed(Uri, Feeds) ->
-    [Feed] = lists:filter(fun(Feed) -> Feed#er_feed.feed =:= Uri end, get_all_feeds(Feeds)),
-    io:format("Updating feed: ~p~n", [Uri]),
-    supervisor:start_child(er_crawler_sup, [Feed]).
